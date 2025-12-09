a2c88e1c0e781bd02fdc22316479f62469b047e3
Angeles Osier <the.crafty.pig.1@gmail.com>
2025-12-09 17:54:05 +0100

    2_AdminWing_Hall added

--- unity_project/My project/Assets/Scripts/angeles/MainMenu.cs
+++ unity_project/My project/Assets/Scripts/angeles/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("1_CylinderRoom_Intro");
+        SceneManager.LoadScene(1);
     }
     public void QuitGame()
     {
